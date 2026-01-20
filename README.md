75b43967425fee69ffa67043726248745d15f005
diegoycge <yunchang_diego_ge@proton.me>
2026-01-20 15:58:25 -0500

    Added OpenFlights dataset import code including original data

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Data used: all fields, 2015 - 2025 from DOT database, downloaded Jan 16, 2026
 <https://www.transtats.bts.gov/Fields.asp?gnoyr_VQ=FMG>
 
 To import the dataset containing all original data, run `source("library_and_data_import.R")` in the Console of RStudio. This should produce `df_all`, which has 5,225,379 observations and 50 variables. This will also import all the libraries you need for subsequent analysis.
+
+OpenFlights data is used for a database of airport names and aircraft names:
+
+<https://openflights.org/data.php>
+
+(accessed Jan 20, 2026)
---
+++ openflights_data_import.R
@@ -0,0 +1,36 @@
+
+# Not sure how to use this in the project yet, but looks like a good dataset
+
+library(readr)
+library(dplyr)
+
+df_openflights_airports <- read_csv("airports.dat",
+                                    col_names = c("airport_id",
+                                                  "name",
+                                                  "city",
+                                                  "country",
+                                                  "iata",
+                                                  "icao",
+                                                  "latitude",
+                                                  "longitude",
+                                                  "altitude",
+                                                  "timezone",
+                                                  "daylight_saving",
+                                                  "timezone_olson",
+                                                  "type",
+                                                  "source"))
+
+df_openflights_planes <- read_csv("planes.dat", 
+                                  col_names = c("name", "iata_code", "icao_code"))
+
+df_airlines <- read_csv("airlines.dat",
+                        col_names = c("airline_id",
+                                      "name",
+                                      "alias",
+                                      "iata_code",
+                                      "icao_code",
+                                      "call_sign",
+                                      "country",
+                                      "active"))
+
+
